330a73752346fc7d1ff16d28d62f931b56eb8075
xudesheng <xudesheng@yahoo.com>
2017-07-29 23:42:09 +0000

    add sql and json example

--- dbdesign/create_oracle_user.sql
+++ dbdesign/create_oracle_user.sql
@@ -2,6 +2,6 @@ create user mesoracle identified by mesoracle
        temporary tablespace TEMP 
        default tablespace USERS       
 /
-grant connect, resource to &&user
+grant connect, resource to mesoracle
 /
 grant create sequence, create view, query rewrite, unlimited tablespace, select any dictionary to mesoracle;
